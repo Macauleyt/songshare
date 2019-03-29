8e1eadbeeafe798d77af91a0bafdce8f1ef70f81
Macauleyt <macauleytreleaven8309@gmail.com>
2019-03-29 13:51:40 +0000

    User Validation Start and Database Models

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://Macauleyt:banana23@ds361085.mlab.com:61085/songshare"
+  mongoURI: "mongodb://Macauleyt:banana23@ds361085.mlab.com:61085/songshare",
+  secretOrKey: "secret"
 };
--- server.js
+++ server.js
@@ -1,27 +1,37 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
 
-const users = require('./routes/api/users');
-const profile = require('./routes/api/profile');
-const posts = require('./routes/api/posts');
+const users = require("./routes/api/users");
+const profile = require("./routes/api/profile");
+const posts = require("./routes/api/posts");
 
 const app = express();
 
-// DB Config
-const db = require('./config/keys').mongoURI;
+//body parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-// Connect to MongoDB
+// Database Connect
+const db = require("./config/keys").mongoURI;
+
 mongoose
   .connect(db)
-  .then(() => console.log('MongoDB Connected'))
+  .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
 
-app.get('/', (req, res) => res.send('Hello World'));
+//passport
+app.use(passport.initialize());
+
+//passport configuration
+
+require("./config/passport")(passport);
 
 // Use Routes
-app.use('/api/users', users);
-app.use('/api/profile', profile);
-app.use('/api/posts', posts);
+app.use("/api/users", users);
+app.use("/api/profile", profile);
+app.use("/api/posts", posts);
 
 const port = process.env.PORT || 5000;
 
